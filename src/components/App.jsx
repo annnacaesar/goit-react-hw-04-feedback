77e0a3582e4fdef7587d45bee5a50fc765738f1e
annnacaesar <Anechka22334@gmail.com>
2022-07-19 13:03:50 +0300

    feat: project settings

---
+++ src/components/App.jsx
@@ -0,0 +1,92 @@
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import { AppContainer } from './App.styled';
+
+export class App extends Component {
+	static propTypes = {};
+
+	state = {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	};
+
+	handleInkremet = event => {
+		const { name } = event.currentTarget;
+
+		this.setState(prevState => ({
+			[name]: prevState[name] + 1,
+		}));
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementGood = () => {
+		this.setState(prevState => {
+			return {
+				good: prevState.good + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementNeutral = () => {
+		this.setState(prevState => {
+			return {
+				neutral: prevState.neutral + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementBad = () => {
+		this.setState(prevState => {
+			return {
+				bad: prevState.bad + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	totalFeedback = () => {
+		const { good, neutral, bad } = this.state;
+		return good + neutral + bad;
+	};
+
+	percentageGoodFeedback = () => {
+		return parseInt((this.state.good / this.totalFeedback()) * 100);
+	};
+	render() {
+		const { good, neutral, bad } = this.state;
+		return (
+			<AppContainer>
+				<Section title="Залиште будь-ласка свій відгук">
+					<FeedbackOptions
+						options={['good', 'neutral', 'bad']}
+						onLeaveFeedback={this.handleInkremet}
+					/>
+				</Section>
+
+				<Section title="Статистика">
+					{good !== 0 || neutral !== 0 || bad !== 0 ? (
+						<Statistics
+							good={good}
+							neutral={neutral}
+							bad={bad}
+							total={this.totalFeedback()}
+							positivePercentage={this.percentageGoodFeedback()}
+						/>
+					) : (
+						<Notification notice='Відгуків поки немає 🙄 '/>
+					)}
+				</Section>
+			</AppContainer>
+		);
+	}
+}
---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Notice = styled.p`
+font-size: 34px;
+color: #666;
+text-align: center;
+height: 260px;
+`
